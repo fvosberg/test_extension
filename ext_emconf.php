f67427068aeb8133ff561caab9962cd099a27998
Frederik Vosberg <frederik.vosberg@diemedialen.de>
2016-07-14 21:39:42 +0200

    first version

---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Test extension',
+    'description' => 'Test extension',
+    'category' => 'extension',
+    'author' => 'Frederik Vosberg',
+    'author_email' => 'hallo@frederikvosberg.de',
+    'shy' => '',
+    'dependencies' => '',
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 1,
+    'lockType' => '',
+    'version' => '1.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-7.6.999'
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'suggests' => array(
+    ),
+);
